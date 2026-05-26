f26c5cc628e5f052ccee28774c13441fc77e20ef
isatousanekvist-ops <isatou.sane.kvist@gmail.com>
2026-05-26 14:29:10 +0200

    Ändrat småfel
    Missade att ändra denna, gick inte att starta app.py alls

--- routes/start_program.py
+++ routes/start_program.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, session, flash
 from db import get_db_connection
 #from utils import login_required
 
-start_program_bp = Blueprint('programs', __name__)
+start_program_bp = Blueprint('start_programs', __name__)
 
 @start_program_bp.route('/start_program/<int:program_id>')
 def start_program(program_id):
@@ -14,5 +14,6 @@ def start_program(program_id):
     user_id = session['user_id']
     conn = get_db_connection()
     cursor = conn.cursor()
+    
 
   
