f955dfa059dc049d3bad9aabed86ea0c55d983fa
isatousanekvist-ops <isatou.sane.kvist@gmail.com>
2026-05-26 13:00:40 +0200

    Uppdatera start_program och view_program
    Lägga till i py om flask. Länka till rätt sida i view_program.

---
+++ routes/start_program.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template, request, redirect, session, flash
+from db import get_db_connection
+#from utils import login_required
+
+programs_bp = Blueprint('programs', __name__)
+
