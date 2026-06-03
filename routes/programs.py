ff161e5901b305eb4506ffa22d45d82c804ab818
isatousanekvist-ops <isatou.sane.kvist@gmail.com>
2026-06-03 04:22:47 +0200

    Fixat småfel
    Löste småfel som jag missat tidigare

--- routes/programs.py
+++ routes/programs.py
@@ -407,13 +407,13 @@ def view_log_workout():
     from utils import get_workout_history 
 
     workout_history = get_workout_history(user_id)
-    
+
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("""
-    SELECT COUNT() FROM peakform.workout_session
+    SELECT COUNT(.) FROM peakform.workout_session
     WHERE user_id = %s
-    AND TO_CHAR(session_date, 'MM-YYYY') = TO_CHAR(NOW(), 'MM-YYYY');
+    AND TO_CHAR(session_date, 'YYYY-MM') = TO_CHAR(NOW(), 'YYYY-MM');
     """, (user_id,))
     antal_pass = cur.fetchone()[0]
     cur.close()
